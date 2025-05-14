6f62e5f99ade5995fb3bf7e68fba724403d512fe
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-05-14 12:33:00 +0300

    feat (navbar) : create navbar and style

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LangService } from '../../../core/services/lang.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  resourcePath = 'navbar.';
+  private langService = inject(LangService);
 
+  changeLang(lang: string) {
+    this.langService.setLang(lang);
+  }
+  get currentLang(): string {
+    return this.langService.currentLang;
+  }
 }
