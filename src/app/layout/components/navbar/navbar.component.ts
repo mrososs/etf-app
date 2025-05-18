a69bc90a562f101fb641381a54346e72351f1177
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-05-19 00:08:02 +0300

    fix : handle responsive and navbar

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,5 +1,13 @@
-import { Component, HostListener, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  ViewChild,
+  ElementRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { LangService } from '../../../core/services/lang.service';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
@@ -9,18 +17,36 @@ import { LangService } from '../../../core/services/lang.service';
 export class NavbarComponent implements OnInit {
   resourcePath = 'navbar.';
   isScrolled = false;
+
   private langService = inject(LangService);
+
+  // ⬅️ النقطة المهمة: استخدام ViewChild عشان نوصل لعنصر collapse
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
+
   ngOnInit(): void {
     window.addEventListener('scroll', this.onScroll, true);
   }
+
   @HostListener('window:scroll', [])
   onScroll = () => {
     this.isScrolled = window.scrollY > 50;
   };
 
+  closeNavbar() {
+    const collapseEl = this.navbarCollapse?.nativeElement;
+    if (collapseEl && window.innerWidth < 992) {
+      const bsCollapse = bootstrap.Collapse.getInstance(collapseEl);
+      if (bsCollapse) {
+        bsCollapse.hide();
+      }
+    }
+  }
+
   changeLang(lang: string) {
     this.langService.setLang(lang);
+    this.closeNavbar();
   }
+
   get currentLang(): string {
     return this.langService.currentLang;
   }
