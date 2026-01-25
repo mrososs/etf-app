25a5a91fb52651d1a1c0001840c978f3cc02f42e
mohamed-osama98 <mmahgoub@adree.com>
2026-01-25 12:43:26 +0200

    Fix issued Mai

--- src/app/shared/components/contact-us-banner/contact-us-banner.component.ts
+++ src/app/shared/components/contact-us-banner/contact-us-banner.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-us-banner',
   templateUrl: './contact-us-banner.component.html',
-  styleUrl: './contact-us-banner.component.scss'
+  styleUrl: './contact-us-banner.component.scss',
 })
 export class ContactUsBannerComponent {
+  translate = inject(TranslateService);
 
+  get mapImage(): string {
+    return this.translate.currentLang === 'en'
+      ? '../../../../assets/img/footerMap2.png'
+      : '../../../../assets/img/footerMap.jpg';
+  }
 }
